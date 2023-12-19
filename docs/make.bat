3d0dacf4df02bbb126611eff913aab1056be5240
Landon Bouma <landonb@users.noreply.github.com>
2023-12-19 13:35:09 -0600

    Deps: Update faithfuls <easy-as-pypi>
    - Source: easy-as-pypi @ 7ee39719fb4a [1.1.14a2~3]
    
    - Commit generated by:
    
        https://github.com/thegittinsgood/git-update-faithful#⛲
    
      under the guidance of:
    
        https://github.com/doblabs/easy-as-pypi/blob/release/bin/update-faithful

--- docs/make.bat
+++ docs/make.bat
@@ -1,10 +1,13 @@
 @ECHO OFF
 
-REM This file exists within 'dob-prompt':
-REM
-REM   https://github.com/tallybark/dob-prompt
+REM vim:tw=0:ts=4:sw=4:noet:norl:nospell:ft=dosbatch:ff=dos
 
-set PROJECTNAME=dob_prompt
+REM Author: Landon Bouma <https://tallybark.com/>
+REM Project: https://github.com/doblabs/dob-prompt
+REM Pattern: https://github.com/doblabs/easy-as-pypi#🥧
+REM License: MIT
+
+set PROJECTNAME=dob-prompt
 
 REM *** END: Project customization. Remainder of file is _boilerplate_.
 
